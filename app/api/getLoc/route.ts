63bf969e465daa7c22a340ea5b6a05160b5ebd7a
Abdillah-MD <ambdi6@gmail.com>
2024-08-24 14:41:54 +0200

    correction api getLoc

--- app/api/getLoc/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import clientPromise from '@/app/lib/mongodb';
-import { ObjectId } from 'mongodb';
-
-export async function GET(request: NextRequest) {
-    console.log("À venir ...")
-}
